cfbd0ef2918fe218bf1c708449651218ae1eadb6
Alex Turcan <alexandru.turcan@softwaremind.com>
2026-03-10 16:30:40 +0200

    Initial Commit

---
+++ nutrx/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nutrx
+//
+//  Created by Alexandru Turcan on 10.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nutrx/nutrxApp.swift
@@ -0,0 +1,17 @@
+//
+//  nutrxApp.swift
+//  nutrx
+//
+//  Created by Alexandru Turcan on 10.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct nutrxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
